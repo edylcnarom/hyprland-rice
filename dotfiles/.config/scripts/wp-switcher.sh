2c78383193314a5a5c1f3e39894a3b21c318e39e
edylcnarom <clydejoshnasatayamorandarte@gmail.com>
2026-04-16 20:35:54 +0800

    matugen initial setup, rofi change, new hyprlock

--- dotfiles/.config/scripts/wp-switcher.sh
+++ dotfiles/.config/scripts/wp-switcher.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Wallpaper directory
-wp_dir="/home/moran/Projects/hyprland-rice/dotfiles/Hyprland-Wallpapers"
+wp_dir="$HOME/Projects/hyprland-rice/dotfiles/Hyprland-Wallpapers"
+symlink_path="$HOME/Projects/hyprland-rice/dotfiles/.config/hypr/current-wp/current_wp"
+
 
 # Current wallpaper
 #'awww query' to find current active wallpaper
-current_wp=$(awww query | awk '{print $NF}')
+current_wp="$HOME/Projects/hyprland-rice/dotfiles/.config/hypr/current-wp/current_wp"
 
 # Random wallpaper
 new_wp=$(find "$wp_dir" -type f | shuf -n 1)
@@ -19,7 +21,12 @@ done
 X_POS="0.$(printf "%02d" $((RANDOM % 100)))"
 Y_POS="0.$(printf "%02d" $((RANDOM % 100)))"
 
-# 5. Change Wallpaper
+## CURSOR_POS=$(hyprctl cursorpos | sed 's/ //g')
+
+# Change Wallpaper
 
 awww img "$new_wp" --transition-type grow --transition-fps 60 --transition-duration 1.5 --transition-pos "$X_POS,$Y_POS"
 
+
+# Create Sym-Link
+ln -sf "$new_wp" "$symlink_path"
